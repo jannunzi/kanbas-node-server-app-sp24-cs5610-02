6551fe50a99a33c05321f55948268c296af96739
Jose Annunziato <jannunzi@gmail.com>
2024-03-27 14:39:45 -0400

    a6

--- App.js
+++ App.js
@@ -4,13 +4,21 @@ import session from "express-session";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
-import CourseRoutes from "./Kanbas/courses/routes.js";
+import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import SecurityController from "./SecurityController.js";
-import UserRoutes from "./Kanbas/users/routes.js";
+import UserRoutes from "./Users/routes.js";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/kanbas-sp24-mon");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(
   session({
--- Courses/routes.js
+++ Courses/routes.js
@@ -1,7 +1,8 @@
-import Database from "../Database/index.js";
+// import Database from "../Kanbas/Database/index.js";
+import * as dao from "./dao.js";
 export default function CourseRoutes(app) {
-  app.get("/api/courses", (req, res) => {
-    const courses = Database.courses;
+  app.get("/api/courses", async (req, res) => {
+    const courses = await dao.findAllCourses();
     res.send(courses);
   });
   app.post("/api/courses", (req, res) => {
@@ -9,10 +10,10 @@ export default function CourseRoutes(app) {
     Database.courses.push(course);
     res.send(Database.courses);
   });
-  app.get("/api/courses/:courseId", (req, res) => {
-    const courses = Database.courses;
+  app.get("/api/courses/:courseId", async (req, res) => {
     const courseId = req.params.courseId;
-    const course = courses.find((course) => course._id === courseId);
+    const course = await dao.findCourseById(courseId); // Database.courses;
+    // const course = courses.find((course) => course._id === courseId);
     res.send(course);
   });
   app.delete("/api/courses/:courseId", (req, res) => {
