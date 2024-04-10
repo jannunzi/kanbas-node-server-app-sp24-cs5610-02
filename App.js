08766aa0d9d34169926872297ec17085b8cfd76b
Jose Annunziato <jannunzi@gmail.com>
2024-04-10 10:36:32 -0400

    cors

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ mongoose.connect(
 const app = express();
 app.use(
   cors({
-    origin: "https://openai--brilliant-entremet-94d513.netlify.app/",
+    origin: "https://openai--brilliant-entremet-94d513.netlify.app",
     credentials: true,
   })
 );
